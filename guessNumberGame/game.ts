6054cf4710bc62d44c22ffc4e0ff593fc4e5406c
S.Mustafa Ahmed <saba.mar88@gmail.com>
2024-03-23 10:51:41 +0500

    update changings in calculator and guess game

--- guessNumberGame/game.ts
+++ guessNumberGame/game.ts
@@ -14,16 +14,16 @@ let userGuess = await inquirer.prompt({
     message: chalk.magentaBright("Guess number:\n choose and type a number(between 0 to 9)."),
 validate: function(gNum) {
     let isValid = !isNaN(gNum) && gNum >= 0 && gNum <= 9;
-    return isValid || 'Invalid input. Please enter a valid number.';
+    return isValid || chalk.yellowBright('Invalid input. Please enter a valid number.');
 }
 });
 
 // if else ststements to print the result
 
 if (userGuess.gNum === num1) {
-    console.log(chalk.bgGreen('congratulations! your guess number is matched.'));
+    console.log(chalk.greenBright('congratulations! your guess number is matched.'));
 } else if (userGuess.gNum === (num1 +1) || userGuess.gNum === (num1 -1)) {
-    console.log (chalk.bgYellowBright('you are closed enough.'));
+    console.log (chalk.yellowBright('you are closed enough.'));
 } else {
-    console.log(chalk.bgRedBright('Sorry! The correct answer was:', num1));
+    console.log(chalk.cyanBright('Sorry! The correct answer was:', num1));
 }
--- simpleCalculator/cal.js
+++ simpleCalculator/cal.js
@@ -24,42 +24,45 @@ let calcu = await inquirer.prompt([
             return isValid || chalk.yellowBright('Please enter a valid number.');
         } }
 ]);
+//convert type input to number
+calcu.num1 = Number(calcu.num1);
+calcu.num2 = Number(calcu.num2);
 //if else statements to print the result according the user's input
 if (calcu.oprator === '+') {
     //if user wanna add values
-    console.log(chalk.magenta(calcu.num1, calcu.oprator, calcu.num2, '=', Number(calcu.num1) + Number(calcu.num2)));
+    console.log(chalk.magenta(calcu.num1, calcu.oprator, calcu.num2, '=', (calcu.num1 + calcu.num2)));
 }
 else if (calcu.oprator === '-') {
     // if user wanna substract 
-    console.log(chalk.greenBright(calcu.num1, calcu.oprator, calcu.num2, '=', Number(calcu.num1) - Number(calcu.num2)));
+    console.log(chalk.greenBright(calcu.num1, calcu.oprator, calcu.num2, '=', (calcu.num1 - calcu.num2)));
 }
 else if (calcu.oprator === '*') {
     //if user want to multiply the values
-    console.log(chalk.redBright(calcu.num1, calcu.oprator, calcu.num2, '=', Number(calcu.num1) * Number(calcu.num2)));
+    console.log(chalk.redBright(calcu.num1, calcu.oprator, calcu.num2, '=', (calcu.num1 * calcu.num2)));
 }
 else if (calcu.oprator === '/') {
     //if user want to divide
     // then check the second Number should not be zero
-    if (Number(calcu.num2) === 0) {
+    if (calcu.num2 === 0) {
         // if the second number is zero, print the statement to user 
         console.log(chalk.bgCyanBright('Can not divided by zero. Please put a non zero second number.'));
     }
     else {
-        console.log(chalk.cyanBright(calcu.num1, calcu.oprator, calcu.num2, '=', Number(calcu.num1) / Number(calcu.num2)));
+        console.log(chalk.cyanBright(calcu.num1, calcu.oprator, calcu.num2, '=', (calcu.num1 / calcu.num2)));
     }
     //if user choose to oprate module
 }
 else if (calcu.oprator === '%') {
     // then check the second Number should not be zero
-    if (Number(calcu.num2) === 0) {
+    if (calcu.num2 === 0) {
         // if the second number is zero, print the statement to user 
         console.log(chalk.bgYellowBright('Can not divided by zero. Please put a non zero second number.'));
     }
     else {
-        console.log(chalk.yellowBright(calcu.num1, calcu.oprator, calcu.num2, '=', Number(calcu.num1) % Number(calcu.num2)));
+        console.log(chalk.yellowBright(calcu.num1, calcu.oprator, calcu.num2, '=', (calcu.num1 % calcu.num2)));
     }
     //if user choose exponant
 }
 else if (calcu.oprator === '**') {
-    console.log(chalk.blueBright(calcu.num1, calcu.oprator, calcu.num2, '=', Number(calcu.num1) ** Number(calcu.num2)));
+    console.log(chalk.blueBright(calcu.num1, calcu.oprator, calcu.num2, '=', (calcu.num1 ** calcu.num2)));
 }
