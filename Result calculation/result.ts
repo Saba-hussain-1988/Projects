3db7ca639b055f973decf72c749d6933f55e6cf9
S.Mustafa Ahmed <saba.mar88@gmail.com>
2024-03-24 11:45:43 +0500

    add program print table

--- Result calculation/result.ts
+++ Result calculation/result.ts
@@ -1,99 +1,110 @@
+#! /usr/bin/env node
+
 import inquirer from "inquirer";
 import chalk from "chalk";
 
 //prompt to take user input
 let marks= await inquirer.prompt([
-    {name: "totalMarksPerPaper", type: "number", message: "enter the total marks of a paper",
+    {name: "totalMarksPerPaper", type: "number",
+     message: chalk.cyanBright("enter the total marks per paper"),
 validate: function(totalMarksPerPaper){
     parseFloat(totalMarksPerPaper);
     let isValid = !isNaN(totalMarksPerPaper) && totalMarksPerPaper<=100 && totalMarksPerPaper>0;
-    return isValid || 'please enter the valid number.';
+    return isValid || chalk.bgMagenta('please enter the valid number:');
 }},
-    {name: "eng", type: "number", message: "English:\nenter obtained marks",
+    {name: "eng", type: "number",
+     message: chalk.blueBright("English:\nenter obtained marks:"),
 validate: function(eng){
     parseFloat(eng);
     let isValid:boolean = !isNaN(eng)  && eng >= 0 ;
-    return isValid || 'please enter the valid number';
+    return isValid || chalk.bgMagenta('please enter the valid number:');
 }},
-{name: "math", type: "number", message: "Mathematics:\nenter obtained marks",
+{name: "math", type: "number", 
+message: chalk.redBright("Mathematics:\nenter obtained marks:"),
 validate: function(math){
     parseFloat(math);
     let isValid = !isNaN(math)  && math >= 0 ;
-    return isValid || 'please enter the valid number';
+    return isValid || chalk.bgMagenta('please enter the valid number:');
 }},
-{name: "sci", type: "number", message: "Science:\nenter obtained marks",
+{name: "sci", type: "number",
+ message: chalk.greenBright("Science:\nenter obtained marks:"),
 validate: function(sci){
     parseFloat(sci);
     let isValid = !isNaN(sci)  && sci >= 0 ;
-    return isValid || 'please enter the valid number';
+    return isValid || chalk.bgMagenta('please enter the valid number:');
 }},
-{name: "comp", type: "number", message: "Computer:\nenter obtained marks",
+{name: "comp", type: "number",
+ message: chalk.yellowBright("Computer:\nenter obtained marks:"),
 validate: function(comp){
     parseFloat(comp);
     let isValid = !isNaN(comp)  && comp >= 0;
-    return isValid || 'please enter the valid number';
+    return isValid || chalk.bgMagenta('please enter the valid number:');
 }},
-{name: "sSt", type: "number", message: "S.Study:\nenter obtained marks",
+{name: "sSt", type: "number", 
+message: chalk.whiteBright("S.Study:\nenter obtained marks:"),
 validate: function(sSt){
     parseFloat(sSt);
     let isValid = !isNaN(sSt)  && sSt>= 0 ;
-    return isValid || 'please enter the valid number';
+    return isValid || chalk.bgMagenta('please enter the valid number:');
 }},
-{name: "urdu", type: "number", message: "Urdu:\nenter obtained marks",
+{name: "urdu", type: "number", 
+message: chalk.hex("Urdu:\nenter obtained marks:"),
 validate: function(urdu){
     parseFloat(urdu);
     let isValid = !isNaN(urdu)  && urdu >= 0 ;
-    return isValid || 'please enter the valid number';
+    return isValid || chalk.bgMagenta('please enter the valid number:');
 }},
-{name: "isl", type: "number", message: "Islamiyat:\nenter obtained marks",
+{name: "isl", type: "number",
+ message: chalk.cyan("Islamiyat:\nenter obtained marks:"),
 validate: function(isl){
     parseFloat(isl);
     let isValid = !isNaN(isl)  && isl >= 0 ;
-    return isValid || 'please enter the valid number';
+    return isValid || chalk.bgMagenta('please enter the valid number:');
 }},
-{name: "sindhi", type: "number", message:chalk.bgMagenta("Sindhi:\nenter obtained marks"),
+{name: "sindhi", type: "number",
+ message:chalk.magenta("Sindhi:\nenter obtained marks"),
 validate: function(sindhi){
     parseFloat(sindhi);
     let isValid = !isNaN(sindhi)  && sindhi >= 0 ;
-    return isValid || 'please enter the valid number';
+    return isValid || chalk.bgMagenta('please enter the valid number');
 }},
 ]);
 
 //print the marks of all subjects one at a time
 for (let i=1; i< marks.length; i++) {
     let subjects=[0, 'English','Mathematices', 'Science', 'Computer', 'S.Study', 'Urdu', 'Islamiyat', 'Sindhi'];
-    console.log(chalk.bgCyanBright(subjects[i]+': ',marks[i]));
+    console.log(chalk.blueBright(subjects[i]+': ',marks[i]));
 }
 
 //show total marks
 let totalMarks= marks.totalMarksPerPaper * 8;
-console.log('Total Marks:  ', totalMarks);
+console.log(chalk.yellowBright('Total Marks:  ', totalMarks));
  
 // print obtained marks
 let obtainedMarks=Number(marks.eng)+ Number(marks.math)+ Number(marks.sci)+ Number(marks.comp)+Number(marks.sSt)+ Number(marks.urdu)+Number(marks.isl)+ Number(marks.sindhi);
-console.log('Total Obtained Marks: ',obtainedMarks);
+console.log(chalk.cyanBright('Total Obtained Marks: ',obtainedMarks));
 
 // show percentage
 let percentage= (obtainedMarks /totalMarks)*100;
-console.log('Percentage: ', percentage+'%');
+console.log(chalk.greenBright('Percentage: ', percentage+'%'));
 
 // grade and remarks
 if (percentage >= 90) {
-    console.log('Grade: A+');
-    console.log('Remarks: Awesome');
+    console.log(chalk.blueBright.bold('Grade: A+'));
+    console.log(chalk.cyanBright('Remarks: Awesome'));
 } else if (percentage <90 && percentage >= 80){
-    console.log('Grade: A');
-    console.log('Remarks: Excellent');   
+    console.log(chalk.blueBright.bold('Grade: A'));
+    console.log(chalk.cyanBright('Remarks: Excellent'));   
 } else if (percentage <80 && percentage >= 70){
-    console.log('Grade: B');
-    console.log('Remarks: Good');   
+    console.log(chalk.blueBright.bold('Grade: B'));
+    console.log(chalk.cyanBright('Remarks: Good'));   
 } else if (percentage <70 && percentage >= 60){
-    console.log('Grade: C');
-    console.log('Remarks: Fine');   
+    console.log(chalk.blueBright.bold('Grade: C'));
+    console.log(chalk.cyanBright('Remarks: Fine'));   
 } else if (percentage <60 && percentage >= 40){
-    console.log('Grade: Pass');
-    console.log('Remarks: Improve your self');   
+    console.log(chalk.blueBright.bold('Grade: Pass'));
+    console.log(chalk.cyanBright('Remarks: Improve your self'));   
 } else if (percentage <40){
-    console.log('Fail');
-    console.log('Remarks: Not satisfying');   
+    console.log(chalk.redBright.bind('Fail'));
+    console.log(chalk.redBright.italic('Remarks: Not satisfying'));   
 }
---
+++ printTable/table.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let input = await inquirer.prompt({
+    name: "num", type: "input",
+    message: chalk.rgb(5, 240, 200)('Please enter your desire number:'),
+    validate: function (num) {
+        let InNum = parseInt(num);
+        let floNum = parseFloat(num);
+        let isValid = !isNaN(InNum) && !isNaN(floNum) && InNum > 0 && floNum > 0;
+        return isValid || chalk.rgb(10, 180, 230)('Invalid entry. Please enter a valid number.');
+    }
+});
+//print table by loop
+for (let i = 1; i <= 10; i++) {
+    console.log(chalk.rgb(50, 110, 250)(input.num, 'x', i, '=', (input.num * i)));
+}
