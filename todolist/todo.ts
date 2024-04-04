9c54e7a107a8936f2c19225dcc1eab8fde3d5db0
S.Mustafa Ahmed <saba.mar88@gmail.com>
2024-04-04 14:41:49 +0500

    changes update in todo and npx package

--- todolist/todo.ts
+++ todolist/todo.ts
@@ -4,134 +4,125 @@ import inquirer from "inquirer";
 import chalk from "chalk";
 
 //create an empty array of toDo list
-let toDoList:string[] = [];
+let todosList:string[] = [];
 
-
-//  "Add ToDos"
+let removedElement:string[] = [];
 
 //create a boolean variable(condition) for the working of the loop
-let condition = true;
+let condition:boolean = true;
+
+//Wel come note
+ console.log(chalk.rgb(250, 0, 255)('Wel Come to your ToDo list!'));
 
 //frequently work of to do addition, i'm using the while loop
-while(condition){
-    //take input from user to add todos
-    let addToDo = await inquirer.prompt(
-        [
+while (condition) {
+
+    //show options of todos and ask to user to choose one of them
+    let optionsList = await inquirer.prompt(
+        {
+            name : 'options', type: 'list', choices: ['Add', 'Edit', 'Delete', 'Undo'],
+            message:  chalk.rgb(245, 138, 250)('select an option which you want to act:'),
+
+            // and also make sure that the input is not blank
+            validate: function(options){
+                let isValid = options.length !== 0 ;
+                return isValid ||  chalk.rgb(245, 138, 250)('Please put some thing in your todo.');
+            }
+        }
+    )
+
+    // ////// "Add ToDos" ////// //
+   
+   // if user choose to add todo
+    if (optionsList.options === 'Add') {
+
+        //take input from user to add todos
+        let addTodo = await inquirer.prompt(
             {
-                name: "toDo", type: "input", message: chalk.cyanBright("Enter your ToDo task:")
+                name : 'add', type:'input',
+                message: chalk.rgb(191, 250, 90)('what do you want to add in your todo list?')
+            }
+        )
+
+        //push usr's input in todos array and print
+        todosList.push(addTodo.add);
+        console.log(chalk.yellowBright("your todo list:"), todosList );
+    }
+//////// "Edit Todos" ////////
+
+// if user want to edit
+    else if (optionsList.options === 'Edit') {
+        let editTodo= await inquirer.prompt(
+          [
+            {
+                // ask to user to choose one of them which he want to edit
+                name: 'select', type: 'list', choices: todosList,
+                message: chalk.rgb(200, 130, 45)('Do you want to edit, select en element:'),
             },
-            //ask to user add more or not
             {
-                name: "isAddMore", type: "confirm", message: chalk.rgb(245, 138, 250)("Do you wanna add one more ToDo?"), default: false
+                //to take new new input from user
+                name: 'edit', type: 'input', message: chalk.rgb(0, 255, 180)('Now you can edit your element:')
             }
-        ],
-        
-    )
-    // update your todolist and print it
-    toDoList.push(addToDo.toDo);
-    console.log(toDoList);
-
-    //check the condition is true, loop will running again
-    condition = addToDo.isAddMore;
-}
-
-//  'Delete'
-
-// an empty array to store all delete elements
-let removedElements:string[] = [];
-
-//create a boolean variable(remove) for the working of the loop
-let remove = true;
-while(remove){
-    // ask to user to remove any element or not
-    let isRemove = await inquirer.prompt(
-        {
-            name: "removed", type: "confirm", message: chalk.rgb(250, 144, 120)("Do you want to remove any task from the list?"), default: false
-        }
-    )
-
-    //if user want to remove
-    if (isRemove.removed){
-
-        let taskName = await inquirer.prompt(
-            {
-                name: "task", type:"list", choices: toDoList,
-                message: chalk.rgb(191, 250, 90)("Select the task which you want to remove:")
-            }
-        )
-        //confirm to user before to delete successfully
-        let isDelete = await inquirer.prompt(
-            {
-                name: "confirmation", type: "confirm", default: true,
-                message: chalk.rgb(230, 80, 100)("Are you sure you want to delete this?")
-            }
-        )
-        //if user confirm to delete code will be worked
-        if (isDelete.confirmation) {
-            let index = toDoList.indexOf(taskName.task);
-            removedElements = toDoList.splice(index, 1);
-            console.log(toDoList);
-            console.log('removed element:', removedElements);
-
-        } else {
-            console.log (toDoList);
-        }
-
-        //  "Undo"
-
-        //ask to user want to undo or not
-        let undo = await inquirer.prompt(
-            {
-                name: "unDo", type: "confirm", default: false,
-                message: chalk.rgb(90, 250, 0)("Do you want to undo your all changes?")
-            }
+          ]  
         )
 
-        //if user want to undo the list
-        if (undo.unDo){
-            toDoList = toDoList.concat(removedElements);
-            console.log(toDoList);
-        }
-        
+        //find the index of the element
+        let index:number = todosList.indexOf(editTodo.select);
+
+        //update the changes in todo list and print
+        todosList.splice(index, 1, editTodo.edit);
+        console.log(todosList);                                                                                                         
     }
-    //check the condition of remove is true, loop will running again
-    remove = isRemove.removed;
-}
 
-//    'EDIt'
+    ////////// "Delete" //////////
+    else if (optionsList.options === 'Delete'){
 
-// an empty array to store all editions
-let editList:string[] = [];
-
-//create a boolean variable(isEdit) for the working of the loop
-let isEdit:boolean = true;
-//loop
-while (isEdit){
-    // ask to user for new edition in the list
-    let editTodo = await inquirer.prompt(
-        
-        {
-            name: 'isMore', type: "confirm", default: false,
-            message: chalk.rgb(191, 250, 90)("Do you want to add anything in the list?")
-        }
-    )
-    //if the value of isEdit is true the if statement will execute
-    isEdit = editTodo.isMore;
-
-    if (isEdit){
-        let editElement = await inquirer.prompt(
-            // if user want to edit , take input from user
+        //// ask to user to choose one of them which he want to delete
+        let deleteTodo = await inquirer.prompt(
             {
-                name: 'edit', type:"input", message: chalk.yellowBright('you can edit your list now:')
+                name: 'delet', type:'list', choices: todosList,
+                message: chalk.rgb(191, 250, 90)('Select which todo you want to delete:')
             }
         )
-    // edition will be a part of an array
-    editList.push(editElement.edit);
-    console.log(editList);
-    
+
+        //find the index of the element which he want to delete
+        let index:number = todosList.indexOf(deleteTodo.delet);
+
+        //delete the element , update and print the todo list
+        removedElement = todosList.splice(index, 1);
+        console.log(todosList);
     }
-    
-    toDoList = toDoList.concat(editList)
-    console.log(toDoList);
-    isEdit = editTodo.isMore;
+
+    //////// "Undo" ////////
+
+    else if(optionsList.options === 'Undo'){
+        let undoTodo = await inquirer.prompt(
+            {
+                //ask to user to confirm tht he want to undo or not
+                name:"undo", type: "confirm", message: chalk.rgb(90, 250, 0)('Do you want to undo all delete elements?')
+            }
+        )
+        if (undoTodo.undo){
+
+            //removed element will become the part of todo again
+           todosList = todosList.concat(removedElement);
+            console.log(todosList);
+        }
+    }
+
+    // ask the user to act some thing again to the list
+   
+    let confirmation = await inquirer.prompt(
+        {
+            name: 'doMore', type: 'confirm', default:true,
+            message: chalk.rgb(230, 80, 100)('Do you want to add more or make changes in your todo?')
+        }
+    )
+    if (!(confirmation.doMore)) {
+        //if not , print the final list to the user 
+        console.log (chalk.yellowBright('Your final list:\n'), todosList);
+    }
+    condition = confirmation.doMore;
 }
+
+//////////////end here/////////
