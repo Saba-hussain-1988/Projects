4d6b5e2e1b6807aea90a064bfc2e06a75349129e
S.Mustafa Ahmed <saba.mar88@gmail.com>
2024-04-28 12:52:42 +0500

    update changes in word counter

--- wordCounter/counter.ts
+++ wordCounter/counter.ts
@@ -20,7 +20,7 @@ const paragraph: {
         message: chalk.rgb(255, 0, 255)("Enter your paragraph which you want to count words and characters.")
     }
 )
-
+//remove the white spaces and extra spaces 
 const words= paragraph.para.trim().split(" ").filter(word => word !== "");
 
 // print the array of words
@@ -29,10 +29,12 @@ console.log(words);
 // count the words of paragraph and print in the console
 console.log(chalk.rgb(0, 240, 255)("There are ", words.length, "words in this paragraph."));
 
+//store all characters in a veriable with out spaces
 const characters = words.join("");
 
+//print the all characters
 console.log(chalk.rgb(200, 230, 90)(characters));
+
+//count and print the number of characters in console
 console.log(chalk.rgb(0, 255, 190)(`There are ${characters.length} characters in this paragraph.`));
-
-
 
