bd431c8210afa334884da00ccc7c7ddb71cc8e6e
S.Mustafa Ahmed <saba.mar88@gmail.com>
2024-07-06 11:06:15 +0500

    save changes

--- advantureGame/game.js
+++ advantureGame/game.js
@@ -1,10 +1,6 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const inquirer_1 = __importDefault(require("inquirer"));
-const chalk_1 = __importDefault(require("chalk"));
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
 //class of player
 class Player {
     constructor(name) {
@@ -25,18 +21,18 @@ class Player {
     //method to show life line
     show_life_line() {
         if (this.fuel == 80) {
-            console.log(`${chalk_1.default.greenBright(this.name + "'s life line:")} ${chalk_1.default.greenBright('________')}__`);
+            console.log(`${chalk.greenBright(this.name + "'s life line:")} ${chalk.greenBright('________')}__`);
         }
         else if (this.fuel == 60) {
-            console.log(`${chalk_1.default.greenBright(this.name + "'s life line:")} ${chalk_1.default.greenBright('______')}____`);
+            console.log(`${chalk.greenBright(this.name + "'s life line:")} ${chalk.greenBright('______')}____`);
         }
         else if (this.fuel == 40) {
-            console.log(`${chalk_1.default.greenBright(this.name + "'s life line:")} ${chalk_1.default.greenBright('____')}______`);
+            console.log(`${chalk.greenBright(this.name + "'s life line:")} ${chalk.greenBright('____')}______`);
         }
         else if (this.fuel == 20) {
-            console.log(`${chalk_1.default.greenBright(this.name + "'s life line:")} ${chalk_1.default.greenBright('__')}________`);
-            console.log(`${chalk_1.default.greenBright(this.name)}${chalk_1.default.whiteBright('! your life is in danger.')}`);
-            console.log(chalk_1.default.whiteBright('Drink portion to get more energy'));
+            console.log(`${chalk.greenBright(this.name + "'s life line:")} ${chalk.greenBright('__')}________`);
+            console.log(`${chalk.greenBright(this.name)}${chalk.whiteBright('! your life is in danger.')}`);
+            console.log(chalk.whiteBright('Drink portion to get more energy'));
         }
     }
     ;
@@ -49,24 +45,24 @@ class Player {
     buy_coins() {
         this.coins += 5;
         this.points -= 20;
-        console.log(chalk_1.default.greenBright.bold(this.name + ':'));
-        console.log(chalk_1.default.greenBright.bold('Coins:', this.coins));
-        console.log(chalk_1.default.greenBright.bold('Points:', this.points));
+        console.log(chalk.greenBright.bold(this.name + ':'));
+        console.log(chalk.greenBright.bold('Coins:', this.coins));
+        console.log(chalk.greenBright.bold('Points:', this.points));
     }
     //method to drink portion
     drink_portion() {
         this.fuel = 100;
         this.coins -= 5;
-        console.log(chalk_1.default.greenBright.bold(this.name + ':'));
-        console.log(chalk_1.default.greenBright.bold('Life line: __________'));
-        console.log(chalk_1.default.greenBright.bold('Coins:', this.coins));
+        console.log(chalk.greenBright.bold(this.name + ':'));
+        console.log(chalk.greenBright.bold('Life line: __________'));
+        console.log(chalk.greenBright.bold('Coins:', this.coins));
     }
     //method to show score
     score_show() {
-        console.log('Player:', chalk_1.default.greenBright.bold(this.name));
+        console.log('Player:', chalk.greenBright.bold(this.name));
         console.log('Life energy:', this.fuel + '%');
-        console.log('Points:', chalk_1.default.greenBright.bold(this.points));
-        console.log('Coins:', chalk_1.default.greenBright.bold(this.coins));
+        console.log('Points:', chalk.greenBright.bold(this.points));
+        console.log('Coins:', chalk.greenBright.bold(this.coins));
     }
 }
 //class of enemy
@@ -85,43 +81,43 @@ class Enemy {
     //method to show life line
     show_life_line() {
         if (this.fuel == 80) {
-            console.log(`${chalk_1.default.redBright(this.name + "'s life line:")} ${chalk_1.default.redBright('________')}__`);
+            console.log(`${chalk.redBright(this.name + "'s life line:")} ${chalk.redBright('________')}__`);
         }
         else if (this.fuel == 60) {
-            console.log(`${chalk_1.default.redBright(this.name + "'s life line:")} ${chalk_1.default.redBright('______')}____`);
+            console.log(`${chalk.redBright(this.name + "'s life line:")} ${chalk.redBright('______')}____`);
         }
         else if (this.fuel == 40) {
-            console.log(`${chalk_1.default.redBright(this.name + "'s life line:")} ${chalk_1.default.redBright('____')}______`);
+            console.log(`${chalk.redBright(this.name + "'s life line:")} ${chalk.redBright('____')}______`);
         }
         else if (this.fuel == 20) {
-            console.log(`${chalk_1.default.redBright(this.name + "'s life line:")} ${chalk_1.default.redBright('__')}________`);
+            console.log(`${chalk.redBright(this.name + "'s life line:")} ${chalk.redBright('__')}________`);
         }
         else if (this.fuel == 0) {
-            console.log(chalk_1.default.redBright(this.name), 'is killed.');
-            console.log(chalk_1.default.blueBright.bold.italic('\tVICTORY'));
+            console.log(chalk.redBright(this.name), 'is killed.');
+            console.log(chalk.blueBright.bold.italic('\tVICTORY'));
         }
     }
     ;
     //method to show score
     show_score() {
-        console.log('Enemy:', chalk_1.default.redBright.bold(this.name));
+        console.log('Enemy:', chalk.redBright.bold(this.name));
         console.log('Life energy:', this.fuel + '%');
     }
 }
 // welcome note.
-console.log(chalk_1.default.rgb(250, 200, 179).bold('-'.repeat(60)));
-console.log(chalk_1.default.rgb(0, 250, 255).bold.italic('\t\t\tWELCOME'));
-console.log(chalk_1.default.rgb(0, 250, 255).bold.italic('\tto Advanture Game'));
-console.log(chalk_1.default.rgb(0, 250, 255).bold('\t\t\t\tBy S.R.Chohan'));
-console.log(chalk_1.default.rgb(250, 200, 179).bold('_'.repeat(60)));
-let _player = await inquirer_1.default.prompt({
+console.log(chalk.rgb(250, 200, 179).bold('-'.repeat(60)));
+console.log(chalk.rgb(0, 250, 255).bold.italic('\t\t\tWELCOME'));
+console.log(chalk.rgb(0, 250, 255).bold.italic('\tto Advanture Game'));
+console.log(chalk.rgb(0, 250, 255).bold('\t\t\t\tBy S.R.Chohan'));
+console.log(chalk.rgb(250, 200, 179).bold('_'.repeat(60)));
+let _player = await inquirer.prompt({
     name: "name",
     type: 'input',
     message: "Enter player name:"
 });
 let condition = true;
 while (condition) {
-    let _enemy = await inquirer_1.default.prompt({
+    let _enemy = await inquirer.prompt({
         name: "name",
         type: "list",
         message: "select your enemy:",
@@ -133,20 +129,20 @@ while (condition) {
         let logic = true;
         // create condition for loop to run rounds
         for (let i = 1; player.fuel > 0 && logic; i++) {
-            console.log(chalk_1.default.magentaBright('~').repeat(30));
-            console.log(chalk_1.default.bgMagenta.bold.italic(`\t\tROUND ${i}`));
-            console.log(chalk_1.default.magentaBright('~').repeat(30));
+            console.log(chalk.magentaBright('~').repeat(30));
+            console.log(chalk.bgMagenta.bold.italic(`\t\tROUND ${i}`));
+            console.log(chalk.magentaBright('~').repeat(30));
             console.log(`\n\t
-                    ${chalk_1.default.bold.bgGreenBright.bold(_player.name)} VS ${chalk_1.default.bold.bgRedBright(_enemy.name)}\n`);
+                    ${chalk.bold.bgGreenBright.bold(_player.name)} VS ${chalk.bold.bgRedBright(_enemy.name)}\n`);
             enemy.fuel = 100;
             while (player.fuel > 0 && enemy.fuel > 0 && logic) {
-                let action = await inquirer_1.default.prompt({
+                let action = await inquirer.prompt({
                     name: 'act',
                     type: "list",
                     message: 'select:',
                     choices: ['Attack', 'Drink Portion', 'Buy coins', 'Score Board', 'Select Enemy']
                 });
-                console.log(chalk_1.default.magentaBright.bold('\t' + '~'.repeat(10)));
+                console.log(chalk.magentaBright.bold('\t' + '~'.repeat(10)));
                 switch (action.act) {
                     case 'Attack':
                         let num = Math.floor(Math.random() * 2);
@@ -158,13 +154,13 @@ while (condition) {
                             enemy.fuel_decrease();
                             enemy.show_life_line();
                             player.points += 10;
-                            console.log(chalk_1.default.greenBright('Get 10 points'));
+                            console.log(chalk.greenBright('Get 10 points'));
                             if (enemy.fuel <= 0) {
                                 player.points += 50;
                                 console.log('You get 50 bonus points');
                             }
                         }
-                        console.log(chalk_1.default.magentaBright.bold('\t' + '~'.repeat(10)));
+                        console.log(chalk.magentaBright.bold('\t' + '~'.repeat(10)));
                         break;
                     case 'Drink Portion':
                         if (player.coins < 5) {
@@ -173,7 +169,7 @@ while (condition) {
                         else {
                             player.drink_portion();
                         }
-                        console.log(chalk_1.default.magentaBright.bold('\t' + '~'.repeat(10)));
+                        console.log(chalk.magentaBright.bold('\t' + '~'.repeat(10)));
                         break;
                     case 'Buy coins':
                         if (player.points < 20) {
@@ -182,16 +178,16 @@ while (condition) {
                         else {
                             player.buy_coins();
                         }
-                        console.log(chalk_1.default.magentaBright.bold('\t' + '~'.repeat(10)));
+                        console.log(chalk.magentaBright.bold('\t' + '~'.repeat(10)));
                         break;
                     case 'Score Board':
                         player.score_show();
                         enemy.show_score();
-                        console.log(chalk_1.default.magentaBright.bold('\t' + '~'.repeat(10)));
+                        console.log(chalk.magentaBright.bold('\t' + '~'.repeat(10)));
                         break;
                     case 'Select Enemy':
                         logic = false;
-                        console.log(chalk_1.default.magentaBright.bold('\t' + '~'.repeat(10)));
+                        console.log(chalk.magentaBright.bold('\t' + '~'.repeat(10)));
                 }
             }
         }
